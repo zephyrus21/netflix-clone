42ce2137826b9a255ca1dbcaa9aee096a182781e
zephyrus21 <pandey.piyus@gmail.com>
2020-08-31 23:31:39 +0530

    build

---
+++ build/precache-manifest.7db837524502c12e546dcbe17625b380.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdecb26595588551f0f09728a1a4f286",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f5c4766f3cb8a8c7232",
+    "url": "/static/css/main.4e4e8aa6.chunk.css"
+  },
+  {
+    "revision": "1e8326e9dd538634541d",
+    "url": "/static/js/2.a9a3c516.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/static/js/2.a9a3c516.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f5c4766f3cb8a8c7232",
+    "url": "/static/js/main.9cf85a1c.chunk.js"
+  },
+  {
+    "revision": "07b33697149324db54e5",
+    "url": "/static/js/runtime-main.b8a87127.js"
+  }
+]);
--- src/Components/Row.js
+++ src/Components/Row.js
@@ -22,7 +22,7 @@ const Row = (props) => {
 
     const opts = {
         height: '390',
-        width: '640',
+        width: '100%',
         playerVars: {
             // https://developers.google.com/youtube/player_parameters
             autoplay: 1,
